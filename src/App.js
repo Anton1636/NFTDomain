8c105a3bc9460cc7e4695c74c8be7a5f6de1294a
Anton1636 <you@example.comhobermac228@gmail.com>
2023-11-30 00:07:00 +0200

    Add frontend part(Navbar,List and Buy Domains)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,82 @@
 import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-
-// Components
+import ETHDaddy from './abis/ETHDaddy.json'
+import Domain from './components/Domain'
 import Navigation from './components/Navigation'
 import Search from './components/Search'
-import Domain from './components/Domain'
+import config from './config.json'
+
+const ethers = require('ethers')
+
+// Components
 
 // ABIs
-import ETHDaddy from './abis/ETHDaddy.json'
 
 // Config
-import config from './config.json';
 
 function App() {
+	const [account, setAccount] = useState(null)
+	const [provider, setProvider] = useState(null)
+	const [ethDomain, setETHDomain] = useState(null)
+	const [domains, setDomains] = useState([])
 
-  return (
-    <div>
+	const loadBlockchainData = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum)
+		setProvider(provider)
 
-      <div className='cards__section'>
+		const network = await provider.getNetwork()
 
-        <h2 className='cards__title'>Welcome to ETH Daddy</h2>
+		const ethDomain = new ethers.Contract(
+			config[network.chainId].ETHDaddy.address,
+			ETHDaddy,
+			provider
+		)
+		setETHDomain(ethDomain)
 
-      </div>
+		const maxSupply = await ethDomain.maxSupply()
+		const domains = []
 
-    </div>
-  );
+		for (var i = 1; i <= maxSupply; i++) {
+			const domain = await ethDomain.getDomain(i)
+			domains.push(domain)
+		}
+		setDomains(domains)
+
+		window.ethereum.on('accountsChanged', async () => {
+			const accounts = await window.ethereum.request({
+				method: 'eth_requestAccounts',
+			})
+			const account = ethers.utils.getAddress(accounts[0])
+			setAccount(account)
+		})
+	}
+
+	useEffect(() => {
+		loadBlockchainData()
+	}, [])
+
+	return (
+		<div>
+			<Navigation account={account} setAccount={setAccount} />
+			<Search />
+			<div className='cards__section'>
+				<h2 className='cards__title'>Domains for you</h2>
+
+				<hr />
+
+				<div className='cards'>
+					{domains.map((domain, index) => (
+						<Domain
+							domain={domain}
+							ethDomain={ethDomain}
+							provider={provider}
+							id={index + 1}
+							key={index}
+						/>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,47 @@
-import { ethers } from 'ethers';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.svg'
+
+const ethers = require('ethers')
 
 const Navigation = ({ account, setAccount }) => {
+	const connectHandler = async () => {
+		const accounts = await window.ethereum.request({
+			method: 'eth_requestAccounts',
+		})
+		const account = ethers.utils.getAddress(accounts[0])
+		setAccount(account)
+	}
 
-  return (
-    <nav>
-
-    </nav>
-  );
+	return (
+		<nav>
+			<div className='nav__link'>
+				<img src={logo} alt='logo' />
+				<h1>ETH Domain</h1>
+				<ul className='nav__links'>
+					<li>
+						<a href='/'>Domains</a>
+					</li>
+					<li>
+						<a href='/'>Hosting</a>
+					</li>
+					<li>
+						<a href='/'>Commerce</a>
+					</li>
+					<li>
+						<a href='/'>Marketing</a>
+					</li>
+				</ul>
+			</div>
+			{account ? (
+				<button type='button' className='nav__connect'>
+					{account.slice(0, 6) + ' ' + account.slice(38, 42)}
+				</button>
+			) : (
+				<button type='button' className='nav__connect' onClick={connectHandler}>
+					Connect
+				</button>
+			)}
+		</nav>
+	)
 }
 
-export default Navigation;
+export default Navigation
