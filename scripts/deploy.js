e06390307780800875898aa97011d17a593a0f82
Anton1636 <you@example.comhobermac228@gmail.com>
2023-11-29 14:50:30 +0200

    Smoke test for NFT Contract, add list domains and buy nft, deploy contract to local network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,20 +1,49 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat')
 
-const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+const tokens = n => {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 async function main() {
+	const [deployer] = await ethers.getSigners()
+	const NAME = 'ETH Daddy'
+	const SYMBOL = 'ETHD'
 
+	const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
+	const ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL)
+	await ethDaddy.deployed()
+
+	console.log(`Deployed Domain Contract at: ${ethDaddy.address}\n`)
+
+	const names = [
+		'test1.domain',
+		'test2.domain',
+		'test3.domain',
+		'test4.domain',
+		'test5.domain',
+		'test6.domain',
+	]
+
+	const costs = [
+		tokens(0.1),
+		tokens(200),
+		tokens(1.2),
+		tokens(0.7),
+		tokens(2.3),
+		tokens(4),
+	]
+
+	for (var i = 0; i < names.length; i++) {
+		const transaction = await ethDaddy
+			.connect(deployer)
+			.list(names[i], costs[i])
+		await transaction.wait()
+
+		console.log(`Listed Domain ${i + 1}: ${names[i]}`)
+	}
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main().catch(error => {
+	console.error(error)
+	process.exitCode = 1
+})
